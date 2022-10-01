9cc259f843bb4ebe9808dff7aa5c1127fc512564
RohanAli12 <alirohan500@gmail.com>
2022-10-01 18:26:20 +0500

    edit

--- Q16.js
+++ Q16.js
@@ -8,9 +8,7 @@ console.log(`${personNames[0]}, can't make the dinner.`);
 
 personNames[0] = 'Salman';
 
-// for (let name of personNames) {
-//     console.log(name + " i am inviting you for dinner.");
-// }
+console.log("I have found Bigger dinning table so i am inviting three more guests.");
 
 personNames.unshift('Adeel');
 let start = 2;
