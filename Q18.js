8c789f1a2873c46128a0dcf954c444a85e5c884e
RohanAli12 <alirohan500@gmail.com>
2022-10-01 19:46:55 +0500

    edit

--- Q18.js
+++ Q18.js
@@ -1,1 +1,19 @@
-console.log("HI")
+let places = ['Mecca', 'Madina', 'Dubai', 'America', 'Naran'];
+let sortingAlp = places.slice().sort();
+let sortingRev = places.slice().sort().reverse();
+
+let reverse = places.slice().reverse();
+let reverse2 = places.slice().reverse().slice().reverse();
+
+console.log(places); //1
+console.log(sortingAlp); //2
+console.log(places); //3
+
+console.log(sortingRev); //4
+console.log(places); //5
+
+console.log(reverse); //6
+console.log(reverse2); //7
+// console.log(places);
+console.log(reverse2.sort());
+console.log(reverse2.sort().reverse());
