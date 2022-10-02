9cb9a1107571ed574c40750b501843e6166834f0
RohanAli12 <alirohan500@gmail.com>
2022-10-02 12:03:13 +0500

    edit

---
+++ Q21.js
@@ -0,0 +1,8 @@
+var person = new Object();
+
+person.firstName = "Rohan";
+person.lastName = "Ali";
+person.age = "18";
+person.qualification = "Undergraduate";
+
+console.log(Object.values(person));
---
+++ Q22.js
@@ -0,0 +1,5 @@
+indexError = [65, "alex", 'dog']
+console.log("Intentional Error calling for del element at -4 which is out of range hehehehehe........");
+
+console.log(indexError[-4]);
+console.log(indexError[0]);
