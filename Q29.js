9bdd01987919ff7eda61929ee3cae787d15e547a
RohanAli12 <alirohan500@gmail.com>
2022-10-03 18:54:49 +0500

    neew

---
+++ Q29.js
@@ -0,0 +1,13 @@
+var favorite_fruits = ['apple', 'banana', 'mango']
+
+if (favorite_fruits.includes('apple')) {
+    console.log("You really like apple!");
+} else if (favorite_fruits.includes('pear')) {
+    console.log("You really like pear!");
+} else if (favorite_fruits.includes('pineapple')) {
+    console.log("You really like pineapple!");
+} else if (favorite_fruits.includes('guava')) {
+    console.log("You really like guava!");
+} else if (favorite_fruits.includes('bees')) {
+    console.log("You really like bees!");
+}
---
+++ Q31.js
@@ -0,0 +1,8 @@
+let username = ['']
+for (let user of username) {
+    if (user == []) {
+        console.log('We need to find some users.');
+    } else {
+        console.log('Welcome back users');
+    }
+}
